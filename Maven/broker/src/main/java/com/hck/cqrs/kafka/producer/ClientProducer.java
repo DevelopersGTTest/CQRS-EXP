0812a1393657cbf911c8b7e793a623bd745c02f5
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-01-30 21:23:53 -0600

    replace by map

--- Maven/broker/src/main/java/com/hck/cqrs/kafka/producer/ClientProducer.java
+++ Maven/broker/src/main/java/com/hck/cqrs/kafka/producer/ClientProducer.java
@@ -4,23 +4,25 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class ClientProducer {
 
     public static void main(String[] args) {
 
-        // base properties
-        Properties props=new Properties();
-        props.put("bootstrap.servers","localhost:9092");
-        props.put("acks","all");
-        props.put("key.serializer",
+        // initial properties..
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put("bootstrap.servers","localhost:9092");
+        configProps.put("acks","all");
+        configProps.put("key.serializer",
                 "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer",
+        configProps.put("value.serializer",
                 "org.apache.kafka.common.serialization.StringSerializer");
 
 
-        try (Producer<String, String> producer = new KafkaProducer<String, String>(props);) {
+        try (Producer<String, String> producer = new KafkaProducer<String, String>(configProps);) {
             //setting test massive messages
             for (int i=0; i < 7000; i++) {
                 producer.send(new ProducerRecord<String, String>("hck-topic", String.valueOf(i), "asd-test-foo"));
