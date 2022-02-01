37248174b0e2ea7837175f4a9e398c2c56d33bc9
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-01-31 23:00:35 -0600

    impl thread and runnable

--- Maven/broker/src/main/java/com/hck/cqrs/kafka/consumer/ClientConsumer.java
+++ Maven/broker/src/main/java/com/hck/cqrs/kafka/consumer/ClientConsumer.java
@@ -11,17 +11,20 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ClientConsumer {
 
-    public static final Logger log = LoggerFactory.getLogger(ClientConsumer.class);
+    private static final Logger log = LoggerFactory.getLogger(ClientConsumer.class);
+
+    private static  final AtomicBoolean nextVal = new AtomicBoolean(true);
 
     public static void main(String[] args) {
 
         // initial properties..
         Map<String, Object> configProps = new HashMap<>();
         configProps.put("bootstrap.servers","localhost:9092");
-        configProps.put("group.id","devs4j-group");
+        configProps.put("group.id","hck-test-group");
         configProps.put("enable.auto.commit","true");
         configProps.put("auto.commit.interval.ms","1000");
         configProps.put("key.deserializer",
@@ -31,7 +34,7 @@ public class ClientConsumer {
 
         try (KafkaConsumer<String, Object> consumer = new KafkaConsumer<>(configProps) ) {
             consumer.subscribe(Arrays.asList("hck-topic"));
-            while(true) {
+            while(nextVal.get()) {
                 ConsumerRecords<String, Object> consumerRecords = consumer.poll(Duration.ofMillis (1000));
                 for (ConsumerRecord<String, Object> consumerRecord : consumerRecords ) {
                     log.info("Offset = {}, Key = {}, Value = {}",
---
+++ Maven/broker/src/main/java/com/hck/cqrs/kafka/thread/ThreadClientConsumer.java
@@ -0,0 +1,51 @@
+package com.hck.cqrs.kafka.thread;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ThreadClientConsumer extends Thread {
+
+    private final KafkaConsumer<String, Object> consumer;
+
+    private static final Logger log = LoggerFactory.getLogger(ThreadClientConsumer.class);
+
+    private static  final AtomicBoolean nextVal = new AtomicBoolean(false);
+
+    public ThreadClientConsumer(KafkaConsumer<String, Object> consumer) {
+        this.consumer = consumer;
+    }
+
+    @Override
+    public void run() {
+        consumer.subscribe(Arrays.asList("hck-topic"));
+        while (!nextVal.get()) {
+            try {
+                ConsumerRecords<String, Object> consumerRecords = consumer.poll(Duration.ofMillis(100));
+                for(ConsumerRecord<String, Object> consumerRecord: consumerRecords) {
+                    log.info("Offset = {}, Key = {}, Value = {}",
+                            consumerRecord.offset(), consumerRecord.key(), consumerRecord.value());
+                }
+            } catch (WakeupException e) {
+                if(!nextVal.get()) {
+                    throw  e;
+                }
+            } finally {
+                consumer.close();
+            }
+        }
+        super.run();
+    }
+
+    public void shutdown() {
+        nextVal.set(true);
+        consumer.wakeup();
+    }
+}
