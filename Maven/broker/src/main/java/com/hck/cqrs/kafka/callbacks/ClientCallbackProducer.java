6516a394c6efd4ea2f919729cd892ff7f2cc7687
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-08-14 20:38:32 -0600

    agent producer for testing OAX env.

--- Maven/broker/src/main/java/com/hck/cqrs/kafka/callbacks/ClientCallbackProducer.java
+++ Maven/broker/src/main/java/com/hck/cqrs/kafka/callbacks/ClientCallbackProducer.java
@@ -1,2 +1,19 @@
-package com.hck.cqrs.kafka.callbacks;public class ClientCallbackProducer {
+package com.hck.cqrs.kafka.callbacks;
+
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class ClientCallbackProducer {
+    @Override
+    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+        if (e != null) {
+            e.printStackTrace();
+        }
+    }
+
+    // Synchronous Message Sending
+    try {
+        Object p  = p.send(record).get();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
 }
