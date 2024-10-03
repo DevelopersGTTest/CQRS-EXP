29a5ccdc3923ac57201a8376542f53db1153e05a
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-10-03 17:17:05 -0600

    initial kafka artifact

---
+++ SpringBoot/kafka-sample-artifact/src/main/java/com/sqrt/kafka_sample_artifact/KafkaSampleArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.sqrt.kafka_sample_artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSampleArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSampleArtifactApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/kafka-sample-artifact/src/test/java/com/sqrt/kafka_sample_artifact/KafkaSampleArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqrt.kafka_sample_artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSampleArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
